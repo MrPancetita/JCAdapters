e3f779b6b44373a063de492936f24a7618e9ad48
MrPancetita <contreras.engineer@outlook.com>
2025-10-15 19:17:46 +0200

    con icon y overline

--- app/src/main/java/net/iessochoa/sergiocontreras/jcadapters/MainScreen.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/jcadapters/MainScreen.kt
@@ -4,8 +4,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,7 +11,6 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.iessochoa.sergiocontreras.jcadapters.ui.components.ItemListAdvance
-import net.iessochoa.sergiocontreras.jcadapters.ui.components.ItemListBasic
 import net.iessochoa.sergiocontreras.jcadapters.ui.components.SpinnerPan
 import net.iessochoa.sergiocontreras.jcadapters.ui.theme.JCAdaptersTheme
 
--- app/src/main/java/net/iessochoa/sergiocontreras/jcadapters/ui/components/ItemList.kt
+++ app/src/main/java/net/iessochoa/sergiocontreras/jcadapters/ui/components/ItemList.kt
@@ -1,18 +1,30 @@
 package net.iessochoa.sergiocontreras.jcadapters.ui.components
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.bumptech.glide.integration.compose.GlideImage
@@ -79,14 +91,38 @@ fun ItemListAdvance(
                 style = Typography.headlineMedium
             )
         },
+        supportingContent = {
+            Text(
+                text = secondaryText,
+                style = Typography.bodyLarge,
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        },
         leadingContent = {
             GlideImage(
                 model = imgUrl,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.size(dimensionResource(R.dimen.img_item_list))
-
+                modifier = Modifier
+                    .size(dimensionResource(R.dimen.img_item_list))
+                    .clip(RoundedCornerShape(dimensionResource(R.dimen.common_padding_default)))
+                    .border(
+                        BorderStroke(
+                            width = dimensionResource(R.dimen.common_padding_nano),
+                            color = colorResource(R.color.blue_200)
+                        ),
+                        shape = RoundedCornerShape(dimensionResource(R.dimen.common_padding_default))
+                    ),
             )
+            //Faltaría el cache strategy que en esta del Glide parece que está bug
+
+        },
+        trailingContent = {
+            Icon(Icons.Default.Info, contentDescription = null)
+        },
+        overlineContent = {
+            Text("Restaurante Feliz")
         }
     )
 
